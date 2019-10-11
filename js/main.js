eb9b532530219ea74c8d3c7479ea66011ca643c1
Kieran Devlin <kjrdevlin@gmail.com>
2019-10-10 18:54:52 -0700

    add flickity plugin carousel and updated email updates form

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // all of your JS code goes here
+document.addEventListener("DOMContentLoaded", function () {
 
 
-
-
-
-
-    
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity(elem, {
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,
+    freeScroll: true,
+    autoPlay: true
   });
+
+
+  const emailForm = document.getElementById("emailForm");
+  const userEmail = document.getElementById("user-email");
+  const button = document.getElementById("submit-button");
+
+  emailForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if (userEmail.value == "") {
+      alert("Please enter a valid email address!");
+    } else {
+      alert("Thank you for subscribing!");
+    }
+
+
+  });
+
+
+
+
+});
