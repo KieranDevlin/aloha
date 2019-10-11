0079ffde02e72879630e2f6acb84672e551eb9ea
Kieran Devlin <kjrdevlin@gmail.com>
2019-10-11 14:24:21 -0700

    add javascript to add to cart btn which counts clicks beside cart icon in navbar - commented js functions - add click counter (add to bag) to html - add add to bag btn to flickity - remove original carousel list items - update btn focus and active pseudo elements

--- js/main.js
+++ js/main.js
@@ -10,11 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
     autoPlay: true
   });
 
-
+  // EMAIL ALERT
   const emailForm = document.getElementById("emailForm");
   const userEmail = document.getElementById("user-email");
   const button = document.getElementById("submit-button");
+  // ADD TO CART COUNTER
+  const buyNow = document.getElementsByClassName("buy-now");
+  const counter = document.getElementById("counter");
+  let clicks =0;
 
+
+  // this creates an alert if the user input is empty or 
+  // doesnt follow the default html email structure validator
+  // and provides a  different alert when user submits a proper
+  // email
   emailForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
@@ -22,10 +31,26 @@ document.addEventListener("DOMContentLoaded", function () {
       alert("Please enter a valid email address!");
     } else {
       alert("Thank you for subscribing!");
+      userEmail.value = "";
     }
 
+  });
+
+
+  // This applies the click function to all 
+  //the buy now buttons in the array
+  // and adds 1 to the click counter beside the 
+  // cart img in html - mimicing a real user ecommerce cart
+  for (let i=0; i < buyNow.length; i++){
+  buyNow[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    clicks += 1;
+    counter.innerHTML = clicks;
+    console.log(clicks);
 
   });
+  };
+
 
 
 
